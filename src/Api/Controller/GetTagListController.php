1656ccddab64b1e0c0b042a06e7d2de953d5807b
strong <dortmundclk@gmail.com>
2023-04-08 18:55:50 +0300

    fix controllers

--- src/Api/Controller/GetTagListController.php
+++ src/Api/Controller/GetTagListController.php
@@ -5,8 +5,11 @@ namespace App\Api\Controller;
 use App\Api\Response\Factory\ApiResponseFactory;
 use App\Repository\TagRepository;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[AsController]
+#[Route(path: '/tag', name: 'get_tags_list', methods: ['GET'])]
 final class GetTagListController
 {
     public function __construct(
@@ -15,7 +18,6 @@ final class GetTagListController
     )
     {}
 
-    #[Route(path: '/tag', name: 'get_tags_list', methods: ['GET'])]
     public function __invoke(): JsonResponse
     {
         return $this->apiResponseFactory->createSuccess(['tags' => $this->tagRepository->findAll()]);
