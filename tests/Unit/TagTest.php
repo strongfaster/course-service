a0c2944df5b8256414dd021b7b62c778886bf416
strong <dortmundclk@gmail.com>
2023-04-08 03:04:23 +0300

    unit test added

---
+++ tests/Unit/TagTest.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace App\Tests\Unit;
+
+use App\Entity\Tag;
+use DateTimeImmutable;
+use PHPUnit\Framework\TestCase;
+
+final class TagTest extends TestCase
+{
+    public function testTagName()
+    {
+        $tag = new Tag();
+        $title = "Tag name";
+
+        $tag->setTitle($title);
+
+        $this->assertEquals($title, $tag->getTitle());
+    }
+
+    public function testUpdatedAt()
+    {
+        $tag = new Tag();
+        $dateTime = new DateTimeImmutable();
+
+        $tag->setUpdatedAt($dateTime);
+
+        $this->assertEquals($dateTime, $tag->getUpdatedAt());
+    }
+}
